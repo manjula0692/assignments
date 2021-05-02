22d0a88ba734287c31781d53c0dc45a036962f57
manjula0692 <manjunov6@gmail.com>
2021-05-02 16:05:15 +0530

    finalday11

--- day11/app.js
+++ day11/app.js
@@ -1,5 +1,17 @@
 const resultEl=document.querySelector(".result");
 const detailsEl=document.querySelector(".details");
+resultEl.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const username = e.target.getAttribute('data-login');
+    if (username) {
+        const items = document.querySelectorAll('.item');
+        items.forEach(item => item.classList.remove('active'));
+
+        e.target.parentElement.parentElement.classList.add('active');
+        fetchRepoOfUser(username);
+    }
+});
+
 
 const fetchUsers=async()=>{
     const response=await fetch('https://api.github.com/users')
@@ -7,18 +19,22 @@ const fetchUsers=async()=>{
     renderUsersList(users);
 
 };
+
 const renderUsersList=(users)=>{
     const trEls=users.map(user=>{
         return `
-        
+        <div class="item">
         <tr>
             <td>${user.login}</td>
             <td><img src="${user.avatar_url}" height="150" alt="${user.login}"></img></td>
              <td>
-                 <button>Show details</button></td>        
+                 <button data-login="${user.login}">Show details</button></td> 
+                      
         </tr>
+        </div>
 `
     }).join('');
+   
     resultEl.innerHTML=`
                 <table>
             <thead>
@@ -31,8 +47,39 @@ const renderUsersList=(users)=>{
      </thead>
      <tbody>
          ${trEls}
+         
      </tbody>
- </table>   
+ </table> 
+ 
 `
 }
+const fetchRepoOfUser=async(login)=>{
+    detailsEl.innerHTML='';
+    const respons=await fetch(`https://api.github.com/users/${login}/repos`)
+    const detail = await respons.json();
+    renderUserDetails(detail);
+
+}
+const renderUserDetails=(detail)=>{
+    const trEls=detail.map(detail=>{
+        return `
+        
+        <tr>
+            <td><li>${detail.name}</li></td>  
+                
+        </tr>
+`
+    }).join('');
+    detailsEl.innerHTML =`
+    <table>
+    <tbody>
+    ${trEls}
+    </tbody>
+    </table>
+
+    `
+
+};
+
+
 fetchUsers();
